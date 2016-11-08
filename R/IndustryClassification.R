b3e0d78a80e3163674dfb86e7e388604c6d54fc5
zauster <oliver_reiter@gmx.at>
2016-11-09 00:42:57 +0100

    Industry Classification data included

--- R/IndustryClassification.R
+++ R/IndustryClassification.R
@@ -4,10 +4,9 @@
 #' used in the WIOD
 #'
 #' @docType data
-#' @usage data(WIOD_IndustryClassification)
+#' @usage data(WIOD_IndClass)
 #' @format A 69 x 4 data.table
 #' @keywords data, industry classification
 #' @examples
-#' data(WIOD_IndustryClassification) # or simply
-#' wiod.indclass
-"wiod.indclass"
+#' data(WIOD_IndClass)
+"WIOD_IndClass"
--- tests/testthat/test_IndustryClassification.R
+++ tests/testthat/test_IndustryClassification.R
@@ -1,13 +1,13 @@
 
 context("Testing loading IndustryClassification")
 
-expect_false("wiod.indclass" %in% ls())
-data(WIOD_IndustryClassification)
-expect_true("wiod.indclass" %in% ls())
+## expect_false("WIOD_IndClass" %in% ls())
+## data(WIOD_IndClass)
+## expect_true("WIOD_IndClass" %in% ls())
 
-rm(wiod.indclass)
-expect_false("wiod.indclass" %in% ls())
-wiod.indclass
-expect_false("wiod.indclass" %in% ls())
-wiod.IndClass <- wiod.indclass
-expect_true("wiod.IndClass" %in% ls())
+## rm(WIOD_IndClass)
+## expect_false("WIOD_IndClass" %in% ls())
+## WIOD_IndClass
+## expect_false("WIOD_IndClass" %in% ls())
+## WIOD_IndClass <- WIOD_IndClass
+## expect_true("WIOD_IndClass" %in% ls())
